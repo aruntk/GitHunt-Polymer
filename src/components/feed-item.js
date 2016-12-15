5049913014be6e9fdece88d1452f4892885b7656
aruntk <aruntknambiar@gmail.com>
2016-12-16 00:03:51 +0530

    ui

--- src/components/feed-item.js
+++ src/components/feed-item.js
@@ -1,9 +1,22 @@
+import moment from 'moment';
+
 class feedItem {
   beforeRegister() {
     this.is = 'feed-item';
     this.properties = Object.assign({}, this.properties, {
       entry: Object,
     });
+  }
+  timeAgo(date) {
+    return moment(date).fromNow();
+  }
+  voteToggle(e) {
+    const fav = e.currentTarget;
+    fav.selected = !fav.selected;
+    fav.classList.toggle('liked');
+    const icon = fav.selected ? 'favorite' : 'favorite-border';
+    fav.setAttribute('icon', icon);
+    // TODO toggle vote
   }
 }
 Polymer(feedItem);
